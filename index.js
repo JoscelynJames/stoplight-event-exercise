bab1704d86dae68752aacef0243927952014b45c
Joscelyn James <josce.james7@hotmail.com>
2017-07-24 15:53:14 -0600

    added color to light

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+const stopBtn = document.getElementById('stopButton');
+const slowBtn = document.getElementById('slowButton');
+const goBtn = document.getElementById('goButton');
+const lights = document.getElementsByClassName('bulb')
+console.log(lights[0]);
 (function() {
   'use strict';
 
-  // YOUR CODE HERE
+stopBtn.addEventListener('click', function(){
+  lights[0].style.background = 'red';
+  stopBtn.addEventListener('click', function(){
+    lights[0].style.background = 'black';
+  })
+})
+
+slowBtn.addEventListener('click', function(){
+  lights[1].style.background = 'yellow';
+  slowBtn.addEventListener('click', function(){
+    lights[1].style.background = 'black';
+  })
+})
+
+goBtn.addEventListener('click', function(){
+  lights[2].style.background = 'green';
+  goBtn.addEventListener('click', function(){
+    lights[2].style.background = 'black';
+  })
+})
 })();
